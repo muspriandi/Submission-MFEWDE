bc49ea45fab63c5a1a8b11fe778b271dbc789c6f
Mus Priandi <1.muspriandi@gmail.com>
2021-05-05 14:54:21 +0700

    add drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,19 +3,36 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 
 const menu = document.querySelector('#menu');
-const hero = document.querySelector('.hero');
-const main = document.querySelector('main');
+const body = document.querySelector('body');
 const drawer = document.querySelector('#drawer');
 
 menu.addEventListener('click', function (event) {
     drawer.classList.toggle('open');
+    body.classList.toggle('shadow');
     event.stopPropagation();
 });
 
-hero.addEventListener('click', function () {
+body.addEventListener('click', function () {
     drawer.classList.remove('open');
+    body.classList.remove('shadow');
 });
 
-main.addEventListener('click', function () {
-    drawer.classList.remove('open');
-});
+
+/*
+*	Working with JavaScript Media Queries (https://css-tricks.com/working-with-javascript-media-queries/)
+*/
+const mediaQuery = window.matchMedia('(min-width: 650px)')
+
+function handleTabletChange(e) {
+	// Check if the media query is true
+	if (e.matches) {
+		// Then log the following message to the console
+		// console.log('Media Query Matched!')
+		drawer.classList.remove('open');
+		body.classList.remove('shadow');
+	}
+}
+// Register event listener
+mediaQuery.addListener(handleTabletChange)
+// Initial check
+handleTabletChange(mediaQuery)
