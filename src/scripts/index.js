c3ac9ab63ba2924b0b5adc0c6dd36e0729517767
Mus Priandi <1.muspriandi@gmail.com>
2021-05-16 15:08:11 +0700

    start make service worker using web pack runtime

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 // eslint-disable-next-line no-unused-vars
 const app = new App({
@@ -19,4 +20,5 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
   app.renderPage();
+  swRegister();
 });
