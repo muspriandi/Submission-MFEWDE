251b2e7096973ed69698a3eaac3475350d0002f1
Mus Priandi <1.muspriandi@gmail.com>
2021-05-17 14:30:33 +0700

    caching with service worker

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+const CONFIG = {
+  KEY: 'YOUR_API_KEY',
+  BASE_URL: 'BASE_URL',
+  BASE_IMAGE_URL: 'BASE_IMG_URL',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
